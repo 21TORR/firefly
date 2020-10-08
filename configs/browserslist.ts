5ddc7bb5f90c71c82d6c743142b25a1198267f83
Jannik Zschiesche <j.zschiesche@21torr.com>
2020-10-08 16:41:10 +0200

    Fix error in browserlist

--- configs/browserslist.ts
+++ configs/browserslist.ts
@@ -1,4 +1,4 @@
 const BASE = "last 2 versions and > 1% and not OperaMini all";
 
-export const BROWSERSLIST_MODERN = `${BASE}, IE 11 and not IE 11`;
+export const BROWSERSLIST_MODERN = `${BASE} and not IE 11`;
 export const BROWSERSLIST_LEGACY = `${BASE}, IE 11`
