7e6bbc67a57ea66170819ad851f9bb849e8e0ba5
Jannik Zschiesche <j.zschiesche@21torr.com>
2020-10-09 11:19:58 +0200

    Fix names

--- tests/fixtures/basic/firefly.js
+++ tests/fixtures/basic/firefly.js
@@ -2,7 +2,7 @@ const {Firefly} = require("../../../src/Firefly");
 
 module.exports = (new Firefly())
 	.js({
-		test_js: "assets/test.js",
+		test: "assets/test.js",
 	})
 	.scss({
 		test: "assets/test.scss",
--- tests/fixtures/typescript/firefly.js
+++ tests/fixtures/typescript/firefly.js
@@ -2,7 +2,7 @@ const {Firefly} = require("../../../src/Firefly");
 
 module.exports = (new Firefly())
 	.js({
-		test_js: "assets/test.ts",
+		test: "assets/test.ts",
 	})
 	.scss({
 		test: "assets/test.scss",
